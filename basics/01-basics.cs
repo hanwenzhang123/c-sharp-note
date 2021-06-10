71ab779e04e33838605259223c612429ff82d1b1
Hanwen Zhang <hanwenzhang888@gmail.com>
2021-06-09 20:38:55 -0400

    Update and rename 01-hello-world.cs to 01-basics.cs

---
+++ basics/01-basics.cs
@@ -0,0 +1,30 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Displays "Hello World!" on the terminal.
+        System.Console.WriteLine("Hello World!");
+    }
+}
+
+//console
+//dotnet run    -The main way to run C# code is using a framework called .NET Core
+Hello World!
+  
+  
+.NET is a platform
+C# is OOP, .NET is functional language
+
+The promise of .NET is the ability to write code once and have it run on any compatible operating system.
+
+
+Compile Errors
+Program.cs(6,49): error CS1002: ; expected [/home/treehouse/workspace/workspace.csproj]
+Program.cs, the name of the file where the error occurred.
+(6, 49), is the line and column number within the code where the error was detected.
+error CS1002, is an error code - missing semicolon
+a brief description of the error: ; expected
+
+Comments
+If you insert two slashes (//) into your code, C# will ignore all the text from there to the end of the line.
+
--- basics/01-hello-world.cs
+++
@@ -1,8 +0,0 @@
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Displays "Hello World!" on the terminal.
-        System.Console.WriteLine("Hello World!");
-    }
-}
